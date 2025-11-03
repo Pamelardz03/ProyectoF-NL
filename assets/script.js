e649d10281736c8c86896e32ead827bcf2b7e6a1
Pamela Rdz <pamela.rodriguezd@udem.edu>
2025-11-03 16:18:42 -0600

    responsivo paginaprincipal

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,19 @@ const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
 menuBtn.onclick = function () {
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
+    if (window.innerWidth < 1024) {
+        if (menu.style.display === 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
     }
 };
 
 document.addEventListener('click', function (e) {
-    if (!menu.contains(e.target) && e.target !== menuBtn) {
-        menu.style.display = 'none';
+    if (window.innerWidth < 1024) {
+        if (!menu.contains(e.target) && e.target !== menuBtn) {
+            menu.style.display = 'none';
+        }
     }
 });
